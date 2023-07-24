9b2e682e301d5aab15061e7bd200df980dc65212
code-with-rskamra/ <robinkamra005@gmail.com>
2023-07-24 21:26:43 +0530

    Updated args and executable path

--- server.js
+++ server.js
@@ -10,7 +10,18 @@ const rateLimit = require("express-rate-limit");
 
 const generateLink = async (url) => {
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: [
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
+      ],
+      executablePath:
+        process.env.NODE_ENV === "production"
+          ? process.env.PUPPETEER_EXECUTABLE_PATH
+          : puppeteer.executablePath(),
+    });
     const page = await browser.newPage();
     await page.goto(url);
 
